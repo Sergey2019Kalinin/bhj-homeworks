5619f34cc146975811bb001c305c5e1377f5ecbf
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-02-06 15:38:18 +0500

    correct 1 HW

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,13 @@
-const timer = document.getElementById("timer");
+let timer = document.getElementById("timer");
 
 setInterval(()  =>  {
 
   timer.textContent = Number(timer.textContent) - 1;
 
-  if (Number(timer.textContent) === 0)  {
+  if (Number(timer.textContent) === 58)  {
     alert("Вы победили!");
+
+    // таймер больше не движется вперёд после нуля
+    timer = 0;
   }
 }, 1000);
