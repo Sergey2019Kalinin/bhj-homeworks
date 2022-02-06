0547c3efcc644f562c0f71a0a53041a7d11cfa63
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-02-06 16:01:51 +0500

    new correct

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,7 +4,7 @@ setInterval(()  =>  {
 
   timer.textContent = Number(timer.textContent) - 1;
 
-  if (Number(timer.textContent) === 58)  {
+  if (Number(timer.textContent) === 0)  {
     alert("Вы победили!");
 
     // таймер больше не движется вперёд после нуля
