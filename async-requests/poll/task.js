729bb7117cb95cf9a5ad2a2edad11076172bbd63
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-05-31 17:11:07 +0500

    well done

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,6 +1,6 @@
 const pollTitle = document.getElementById("poll__title");
 const pollAnswers = document.getElementById("poll__answers");
-      pollAnswers.style.display = "flex";
+pollAnswers.style.display = "flex";
 
 const answers = new XMLHttpRequest();
 
@@ -10,28 +10,26 @@ answers.responseType = "text";
 
 answers.send();
 
-answers.addEventListener("readystatechange", () => {
-  if (answers.readyState === answers.DONE) {
-    let answer = JSON.parse(answers.responseText);
+answers.onload = function () {
+  let answer = JSON.parse(answers.responseText);
 
-    const answerTitle = document.createElement("h2");
-    answerTitle.innerHTML = answer.data.title;
-    pollTitle.appendChild(answerTitle);
+  const answerTitle = document.createElement("h2");
+  answerTitle.innerHTML = answer.data.title;
+  pollTitle.appendChild(answerTitle);
 
-    let optionsArray = Array.from(answer.data.answers);
+  let optionsArray = Array.from(answer.data.answers);
 
-    for (let i = 0; i < optionsArray.length; i++ ) {
+  for (let i = 0; i < optionsArray.length; i++) {
+    let addOption = document.createElement("button");
 
-      let addOption = document.createElement("button");
+    addOption.innerText = `${optionsArray[i]}`;
+    addOption.style.marginRight = "10px";
+    addOption.classList.add("poll__answer");
 
-      addOption.innerText = `${optionsArray[i]}`;
-      addOption.style.marginRight = "10px";
-      addOption.classList.add("poll__answer");
+    pollAnswers.appendChild(addOption);
 
-      pollAnswers.appendChild(addOption);
-
-    }
-}
-
-});
-
+    addOption.addEventListener("click", () => {
+      alert("«Спасибо, ваш голос засчитан!»");
+    });
+  }
+};
--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -2,6 +2,7 @@ const loader = document.getElementById("loader");
 
 // место, куда добавляется
 const item = document.getElementsByClassName("item")[0];
+item.style.flexDirection = "column";
 
 const data = new XMLHttpRequest();
 
@@ -13,34 +14,32 @@ data.onload = function () {
   if (data.status === 200) {
     loader.classList.remove("loader_active");
 
-    let valuteList = JSON.parse(this.responseText);
+    let valuteList = JSON.parse(data.responseText);
     let dataObj = valuteList.response.Valute;
 
-    let rubText = document.createElement("div");
-    rubText.className = "item__currency";
-    rubText.textContent = "руб.";
-
     for (let valute in dataObj) {
+      let value = document.createElement("div");
+      value.className = "item__value";
+      value.textContent = dataObj[valute].Value;
+
+      let charCode = document.createElement("div");
+      charCode.className = "item__code";
+      charCode.textContent = dataObj[valute].CharCode;
+
+      let rubText = document.createElement("div");
+      rubText.className = "item__currency";
+      rubText.textContent = "руб.";
+
       let newItemCode = document.createElement("div");
       newItemCode.style.display = "flex";
 
-      let Value = document.createElement("div");
-      Value.className = "item__value";
-      Value.textContent = dataObj.AMD.Value;
-
-      let CharCode = document.createElement("div");
-      CharCode.className = "item__code";
-      CharCode.textContent = dataObj.AMD.CharCode;
-
-      newItemCode.appendChild(CharCode);
-      newItemCode.appendChild(Value);
+      newItemCode.appendChild(charCode);
+      newItemCode.appendChild(value);
       newItemCode.appendChild(rubText);
 
       item.appendChild(newItemCode);
     }
   } else {
-    alert("Ошибка: " + this.status);
+    alert("Ошибка: " + data.status);
   }
-
-  return;
 };
