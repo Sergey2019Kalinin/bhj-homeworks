d44a75e714171101ce490c5e446fe4e89be44372
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-06-13 13:05:47 +0500

    progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,32 +2,34 @@ const progress = document.getElementById("progress");
 
 const form = document.getElementById("form");
 
-form.addEventListener("submit", function (ev) {
+let progressValue = function (XMLHttpReq) {
+  XMLHttpReq.upload.onprogress = function (event) {
+    progress.value = event.loaded / event.total;
+  };
+};
 
-	const oData = new FormData(form);
+form.addEventListener(
+  "submit",
+  function (ev) {
+    const oData = new FormData(form);
 
-	const oReq = new XMLHttpRequest();
+    const oReq = new XMLHttpRequest();
 
     oReq.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
 
-    oReq.send(oData);
+    progressValue(oReq);
 
     oReq.onloadend = function () {
-      if (oReq.status == 200) {
+      if (oReq.status === 200) {
         console.log("Успех");
       } else {
         console.log("Ошибка " + this.status);
       }
     };
 
-
+    oReq.send(oData);
 
     ev.preventDefault();
-    
-  },false);
-
-// не понимаю как обратиться к запросу вне обработчика
-    oReq.upload.onprogress = function (event) {
-      progress.value = event.loaded/event.total;
-    };
-
+  },
+  false
+);
